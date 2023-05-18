4713cd17346cb11317bafe976c46d1ce944ea4a8
Rafael Medeiros <rafaelmedrib@gmail.com>
2023-05-18 20:20:09 -0300

    Add login page

--- web/src/Routes.tsx
+++ web/src/Routes.tsx
@@ -16,6 +16,10 @@ import { useAuth } from './auth'
 const Routes = () => {
   return (
     <Router useAuth={useAuth}>
+      <Route path="/login" page={LoginPage} name="login" />
+      <Route path="/signup" page={SignupPage} name="signup" />
+      <Route path="/forgot-password" page={ForgotPasswordPage} name="forgotPassword" />
+      <Route path="/reset-password" page={ResetPasswordPage} name="resetPassword" />
       <Set wrap={ScaffoldLayout} title="Employees" titleTo="employees" buttonLabel="New Employee" buttonTo="newEmployee">
         <Route path="/employees/new" page={EmployeeNewEmployeePage} name="newEmployee" />
         <Route path="/employees/{id:Int}/edit" page={EmployeeEditEmployeePage} name="editEmployee" />
